51c6b3556a64e44f35ef28a0131990d33c9ef79e
Santosh Singh <santosh.singh@gatech.edu>
2023-08-14 14:22:38 -0400

    added selection sort algorithm

---
+++ sorting/selection_sort.py
@@ -0,0 +1,30 @@
+"""
+Asymptotic complexity in terms of size of 'arr' 'n':
+* Time: O(n ^ 2).
+* Auxiliary space: O(1).
+* Total space: O(n).
+"""
+
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        # Assume the smallest element is the current element
+        min_index = i
+        # Iterate through the unsorted part of the list
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                # Update the min_index if a smaller element is found
+                min_index = j
+        # Swap the found minimum element with the first element of the unsorted part
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+
+if __name__ == "__main__":
+    arr = [5, 8, 3, 9, 4, 1, 7]
+    sorted_arr = selection_sort(arr)
+    if arr == sorted_arr:
+        print(f'Output - {sorted_arr} => Test pass!')
+    else:
+        print(f'Output - {sorted_arr} => Test failed!')
