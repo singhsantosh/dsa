267af5c7c12ba0efa111dcd4a4e76b6a0bbdef1c
Santosh Singh <santosh.singh@gatech.edu>
2023-11-07 13:55:30 -0500

    midified the valid_palindrome code

--- blind75/two-pointer/valid_palindrome.py
+++ blind75/two-pointer/valid_palindrome.py
@@ -1,19 +1,33 @@
+# leetcode problem : https://leetcode.com/problems/valid-palindrome/description/
 # time  : O(n)
 # space : O(1)
-def is_palindrome(s: list) -> bool:
-    left = 0
-    right = len(s) - 1
-    while left < right:
-        if s[left] != s[right]:
-            return False
-        left += 1
-        right -= 1
+class Solution:
+    def is_palindrome(self, s: str) -> bool:
+        s = s.lower()
+        if len(s) == 0:
+            return True
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            while l < r and not self.isalphanum(s[l]):
+                l += 1
 
-    return True
+            while l < r and not self.isalphanum(s[r]):
+                r -= 1
+
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+
+        return True
+
+    def isalphanum(self, c: str) -> bool:
+        return ('a' <= c <= 'z' or '0' <= c <= '9')
 
 
 def main():
-
+    s = Solution()
     test_cases = ["RACEACAR", "A", "ABCDEFGFEDCBA",
                   "ABC", "ABCBA", "ABBA", "RACEACAR"]
     for i in range(len(test_cases)):
@@ -21,7 +35,7 @@ def main():
         print("-" * 100)
         print("The input string is '", test_cases[i], "' and the length of the string is ", len(
             test_cases[i]), ".", sep='')
-        print("Is it a palindrome?.....", is_palindrome(test_cases[i]))
+        print("Is it a palindrome?.....", s.is_palindrome(test_cases[i]))
         print("-" * 100)
 
 
